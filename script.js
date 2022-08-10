fa63b3203b144eeb849c4ddb6ae64dc9c87d03f5
Rachael-Minage <lauryneminagge@gmail.com>
2022-08-10 18:13:16 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+var hopperLab = {
+    name: "bar",
+    func: function() {
+        var self = this;
+        console.log("outer func:  this.name = " + this.name);
+        console.log("outer func:  self.name = " + self.name);
+        (function() {
+            console.log("inner func:  this.name = " + this.name);
+            console.log("inner func:  self.name = " + self.name);
+        }());
+    }
+};
+
+hopperLab.func();
